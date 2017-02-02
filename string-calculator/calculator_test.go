e78a8df74a9544db46d59e067bf81ae14b3b25f7
codingconcepts <bassrob@gmail.com>
2017-02-02 08:28:37 +0000

    Solved Q2

--- string-calculator/calculator_test.go
+++ string-calculator/calculator_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_SimpleCalculator_AddValid(t *testing.T) {
+func Q1_Test_Calculator_Add(t *testing.T) {
 	testCases := map[string]int{
 		"":        0,
 		"0":       0,
@@ -19,7 +19,7 @@ func Test_SimpleCalculator_AddValid(t *testing.T) {
 	sut := Calculator{}
 
 	for key, value := range testCases {
-		t.Run(fmt.Sprintf("Test_SimpleCalculator_AddValid %q=%v", key, value), func(t *testing.T) {
+		t.Run(fmt.Sprintf("Test_Calculator_Add %q=%v", key, value), func(t *testing.T) {
 			actual, err := sut.Add(key)
 
 			if err != nil {
@@ -32,7 +32,7 @@ func Test_SimpleCalculator_AddValid(t *testing.T) {
 	}
 }
 
-func Test_SimpleCalculator_AddInvalid(t *testing.T) {
+func Q1_Test_Calculator_AddInvalid(t *testing.T) {
 	testCases := []string{
 		",",
 		",1,",
@@ -49,7 +49,48 @@ func Test_SimpleCalculator_AddInvalid(t *testing.T) {
 	sut := Calculator{}
 
 	for _, value := range testCases {
-		t.Run(fmt.Sprintf("Test_SimpleCalculator_AddInvalid %q", value), func(t *testing.T) {
+		t.Run(fmt.Sprintf("Test_Calculator_AddInvalid %q", value), func(t *testing.T) {
+			_, err := sut.Add(value)
+			if err != missingNumberError {
+				t.Fatalf("expected %v but got %v", missingNumberError, err)
+			}
+		})
+	}
+}
+
+func Q2_Test_Calculator_AddMoreThan3(t *testing.T) {
+	testCases := map[string]int{
+		"1, 2, 3, 4":                   10,
+		"1, 1, 1, 1, 1, 1, 1, 1, 1, 1": 10,
+	}
+
+	sut := Calculator{}
+
+	for key, value := range testCases {
+		t.Run(fmt.Sprintf("Q2_Test_Calculator_AddMoreThan3 %q=%v", key, value), func(t *testing.T) {
+			actual, err := sut.Add(key)
+
+			if err != nil {
+				t.Fatalf("expected no error but got %v", err)
+			}
+			if actual != value {
+				t.Fatalf("expected %v but got %v", value, actual)
+			}
+		})
+	}
+}
+
+func Q2_Test_Calculator_AddMoreThan3Invalid(t *testing.T) {
+	testCases := []string{
+		",1, 2, 3, 4",
+		"1, 2, 3, 4,",
+		",1, 2, 3, 4,",
+	}
+
+	sut := Calculator{}
+
+	for key, value := range testCases {
+		t.Run(fmt.Sprintf("Q2_Test_Calculator_AddMoreThan3Invalid %q=%v", key, value), func(t *testing.T) {
 			_, err := sut.Add(value)
 			if err != missingNumberError {
 				t.Fatalf("expected %v but got %v", missingNumberError, err)
